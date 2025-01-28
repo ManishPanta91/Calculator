3d2b2168f7dd5241754ca4349cc4c6c5b5e147b2
Manish Panta <158488852+ManishPanta91@users.noreply.github.com>
2025-01-28 20:23:32 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const input = document.getElementById("input");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const buttonText = button.textContent;
+
+        if (buttonText === "AC") {
+            input.value = "";
+        } else if (buttonText === "DEL") {
+            input.value = input.value.slice(0, -1);
+        } else if (buttonText === "=") {
+            try {
+                input.value = eval(input.value);
+            } catch {
+                input.value = "Error";
+            }
+        } else {
+            input.value += buttonText;
+        }
+    });
+});
